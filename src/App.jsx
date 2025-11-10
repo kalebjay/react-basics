709c889f55d64c014cbe7fb09154da72526878ad
kaleb <kalebjay@gmail.com>
2025-11-10 12:35:04 -0500

    active components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,73 @@
 import { useState } from 'react';
-import { CORE_CONCEPTS } from './data.js'; 
+import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header.jsx';
 import CoreConcept from './components/CoreConcepts.jsx';
 import TabButton from './components/TabButton.jsx';
 import { EXAMPLES } from './data.js';
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('components');
+  const [selectedTopic, setSelectedTopic] = useState();
 
   function handleSelect(selectedButton) {
     // selectedButton => 'Components', 'JSX', 'Props', 'State'
     setSelectedTopic(selectedButton);
     console.log(selectedTopic);
   }
-  
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+
   return (
     <div>
       <Header />
       <main>
         <section id="core-concepts">
-        <h2>Core Concepts</h2>
-        <ul>
-          {/* Original way, .notation */}
-          <CoreConcept 
-            title={CORE_CONCEPTS[0].title}
-            description={CORE_CONCEPTS[0].description} 
-            image={CORE_CONCEPTS[0].image}
-          /> 
-          {/* New way, using destructuring */}
-          <CoreConcept {...CORE_CONCEPTS[1]}/>
-          <CoreConcept {...CORE_CONCEPTS[2]}/>
-          <CoreConcept {...CORE_CONCEPTS[3]}/>
-        </ul>
+          <h2>Core Concepts</h2>
+          <ul>
+            {/* Original way, .notation */}
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            {/* New way, using destructuring */}
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton> 
-            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+            <TabButton isSelected={selectedTopic === 'components'}
+              onSelect={() => handleSelect('components')}>
+              Components
+            </TabButton>
+            <TabButton isSelected={selectedTopic === 'jsx'}
+              onSelect={() => handleSelect('jsx')}>
+              JSX
+            </TabButton>
+            <TabButton isSelected={selectedTopic === 'props'}
+              onSelect={() => handleSelect('props')}>
+              Props
+            </TabButton>
+            <TabButton isSelected={selectedTopic === 'state'}
+              onSelect={() => handleSelect('state')}>
+              State
+            </TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>
+          {tabContent}
         </section>
       </main>
     </div>
